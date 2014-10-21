c6fd826ed325d3c4910242478ea8c24e153832fb
Rex Lee <duguying2008@gmail.com>
2014-10-22 00:57:59 +0800

    拦截syscall, 初见成效

--- src/nix/judger.cpp
+++ src/nix/judger.cpp
@@ -2,35 +2,60 @@
 
 int main(int argc, char *argv[])
 {
-  struct user_regs_struct reg;
-  int syscall;
-
   pid_t child;
   long orig_eax;
+
+  if(argc<2){
+    printf("Usage: judger <command> \n");
+    return 0;
+  }
+
   child = fork();
-
-  ptrace(PTRACE_GETREGS,child,NULL,&reg);
-  #ifdef __i386__
-  syscall = reg.orig_eax;
-  #else
-  syscall = reg.orig_rax;
-  #endif
-
-  printf("syscall is %d\n", syscall);
-
   if(child == 0) {
-     ptrace(PTRACE_TRACEME, 0, NULL, NULL);
-     execl("/bin/ls", "ls", NULL);
+    ptrace(PTRACE_TRACEME, 0, NULL, NULL);
+    execvp(argv[1], argv+1);
+    exit(0);
   }else{
-     wait(NULL);
-     printf("the child pid is %d\n", child);
-     
-     orig_eax = ptrace(PTRACE_PEEKUSER, 
-                       child, 4 * ORIG_EAX, 
-                       NULL);
+    struct rusage rinfo;
+    int runstat;
 
-     printf("The child made a system call %ld\n", orig_eax);
-     ptrace(PTRACE_CONT, child, NULL, NULL);
+    printf("the child pid is %d\n", child);
+
+    for(;;){
+      wait4(child,&runstat,0,&rinfo);
+
+      if (WIFEXITED(runstat))
+      {
+        exit(0);
+      }
+      else if (WIFSIGNALED(runstat))
+      {
+        //result(RS_ECR,SIGKILL);
+      }
+      else if (WIFSTOPPED(runstat))
+      {
+        if (WSTOPSIG(runstat) == SIGTRAP){
+          struct user_regs_struct reg;
+          int syscall;
+          static int executed = 0;
+          int forbidden = 0;
+                
+          ptrace(PTRACE_GETREGS,child,NULL,&reg);
+          #ifdef __i386__
+          syscall = reg.orig_eax;
+          #else
+          syscall = reg.orig_rax;
+          #endif
+                      
+          printf("syscall: %d\n",syscall);
+        }
+      }
+
+//      orig_eax = ptrace(PTRACE_PEEKUSER, child, 4 * ORIG_EAX, NULL);
+//      printf("The child made a system call %ld\n", orig_eax);
+      ptrace(PTRACE_SYSCALL, child, NULL, NULL);
+    }
   }
   return 0;
 }
+
---
+++ src/nix/test.cpp
@@ -0,0 +1,32 @@
+/**
+ * File Name: test.cpp
+ * Author: rex
+ * Mail: duguying2008@gmail.com 
+ * Created Time: 2014年10月21日 星期二 23时05分12秒
+ */
+
+#include <iostream>
+#include "stdio.h"
+#include "stdlib.h"
+using namespace std;
+
+struct mystruct
+{
+  int i;
+  char cha;
+};
+
+int main(void){
+  FILE *stream;
+  struct mystruct s;
+  if ((stream = fopen("TEST.$$$", "wb")) == NULL) /* open file TEST.$$$ */
+  {
+    fprintf(stderr, "Cannot open output file.\n");
+    return 1;
+  }
+  s.i = 0;
+  s.cha = 'A';
+  fwrite(&s, sizeof(s), 1, stream); /* 写的struct文件*/
+  fclose(stream); /*关闭文件*/
+  return 0;
+}
