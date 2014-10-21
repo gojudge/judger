1ca3dffa82e4fa64454de9dab164cf7a23d9a30f
Rex Lee <duguying2008@gmail.com>
2014-10-21 22:07:41 +0800

    test trace syscall

--- src/nix/judger.cpp
+++ src/nix/judger.cpp
@@ -1,7 +1,36 @@
 #include "judger.h"
 
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
-	/* code */
-	return 0;
+  struct user_regs_struct reg;
+  int syscall;
+
+  pid_t child;
+  long orig_eax;
+  child = fork();
+
+  ptrace(PTRACE_GETREGS,child,NULL,&reg);
+  #ifdef __i386__
+  syscall = reg.orig_eax;
+  #else
+  syscall = reg.orig_rax;
+  #endif
+
+  printf("syscall is %d\n", syscall);
+
+  if(child == 0) {
+     ptrace(PTRACE_TRACEME, 0, NULL, NULL);
+     execl("/bin/ls", "ls", NULL);
+  }else{
+     wait(NULL);
+     printf("the child pid is %d\n", child);
+     
+     orig_eax = ptrace(PTRACE_PEEKUSER, 
+                       child, 4 * ORIG_EAX, 
+                       NULL);
+
+     printf("The child made a system call %ld\n", orig_eax);
+     ptrace(PTRACE_CONT, child, NULL, NULL);
+  }
+  return 0;
 }
--- src/nix/judger.h
+++ src/nix/judger.h
@@ -1,2 +1,13 @@
-#include "stdio.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <math.h>
+#include <sys/reg.h>
+#include <sys/user.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <sys/wait.h>
 #include <sys/ptrace.h>
+#include <sys/syscall.h>
