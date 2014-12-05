43d24e34558bc6d035da020dcb2f4e5b1c2fe17d
Rex Lee(李俊) <duguying2008@gmail.com>
2014-12-06 00:18:04 +0800

    add run.sh

---
+++ script/run.sh
@@ -0,0 +1,8 @@
+# 沙箱运行调用脚本
+# %4 is the directory of executable
+# %1 is executable relative
+# %2 is -t=time
+# %3 is -m=mem
+
+cd %4
+/root/gopath/src/github.com/duguying/judger/sandbox/c/build/executer %1 %2 %3 > RUN.LOG
