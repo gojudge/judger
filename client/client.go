ffb6b638f407da4f0b03a7bdaf8d73bfc2f06033
Rex Lee <duguying2008@gmail.com>
2015-01-10 12:52:48 +0800

    finished client api

--- client/client.go
+++ client/client.go
@@ -9,16 +9,33 @@ import (
 type JClient struct {
 	ip   string
 	port int
+	conn net.Conn
 }
 
-func (this *JClient) Start(ip string, port int) net.Conn {
+func (this *JClient) Start(ip string, port int) error {
 	addr := fmt.Sprintf("%s:%d", ip, port)
 	conn, err := net.Dial("tcp", addr)
 	if err != nil {
 		conn.Close()
 		log.Warnln("connect judge server failed in port:", port)
-		return nil
 	} else {
-		return conn
+		this.conn = conn
 	}
+
+	return err
+}
+
+func (this *JClient) Request(msg string) string {
+	var buf [10240]byte
+
+	this.conn.Write(msg)
+
+	n, err := this.conn.Read(buf[0:])
+	if err != nil {
+		if err == io.EOF {
+			return
+		}
+	}
+
+	return string(buf[0:n])
 }
