2d331f51d623ddeceefc8549be0cdab630ff7ff7
Rex Lee <duguying2008@gmail.com>
2015-01-21 20:17:48 +0800

    fixed client library

--- client/client.go
+++ client/client.go
@@ -1,6 +1,7 @@
 package client
 
 import (
+	"errors"
 	"fmt"
 	"github.com/gogather/com/log"
 	"io"
@@ -37,26 +38,31 @@ func (this *JClient) Start(ip string, port int) error {
 		log.Warnln("connect judge server failed in port:", port)
 	} else {
 		this.conn = conn
+		content, _ := this.read()
+		log.Blueln(content)
 	}
 
 	return err
 }
 
-func (this *JClient) Request(msg string) string {
-	var buf [10240]byte
-
+func (this *JClient) Request(msg string) (string, error) {
 	if !this.connected {
-		return ""
+		return "", errors.New("Not Connected!")
 	}
-
 	this.conn.Write([]byte(msg))
+	content, err := this.read()
+	return content, err
+}
+
+func (this *JClient) read() (string, error) {
+	var buf [10240]byte
 
 	n, err := this.conn.Read(buf[0:])
 	if err != nil {
 		if err == io.EOF {
-			return ""
+			return "", err
 		}
 	}
 
-	return string(buf[0:n])
+	return string(buf[0:n]), err
 }
