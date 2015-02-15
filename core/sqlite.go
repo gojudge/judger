38748bf99469c87935977f3a945e7ff758233c41
Rex Lee <duguying2008@gmail.com>
2015-02-15 18:19:26 +0800

    use beego orm for sqlite driver

--- core/sqlite.go
+++ core/sqlite.go
@@ -1,53 +1,27 @@
 package core
 
 import (
-	"database/sql"
 	"fmt"
+	"github.com/astaxie/beego/orm"
 	_ "github.com/mattn/go-sqlite3"
-	"path/filepath"
 )
 
 type Sqlite struct {
 	Filename string
-	db       *sql.DB
 }
 
 func (this *Sqlite) NewSqlite(dataPath string) {
-	var err error
-	this.Filename = filepath.Join(dataPath)
+	orm.RegisterDataBase("default", "sqlite3", "data.db")
 
-	this.db, err = sql.Open("sqlite3", this.Filename)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	if this.checkTableExist("foo") {
-
-	}
+	this.createTable()
 }
 
-// check whether the table exist
-func (this *Sqlite) checkTableExist(tableName string) bool {
-	sql := `SELECT count(*) as num FROM sqlite_master WHERE type='table' AND name='` + tableName + `'`
-	fmt.Println(sql)
+func (this *Sqlite) createTable() {
+	o := orm.NewOrm()
 
-	rows, err := this.db.Query(sql)
+	_, err := o.Raw("CREATE TABLE [foo] ([id] INTEGER  NOT NULL PRIMARY KEY)").Exec()
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	var num int
-	rows.Scan(&num)
-	if num == 0 {
-		return false
-	} else {
-		return true
-	}
-}
-
-// execute sql
-func (this *Sqlite) Exec(sql string) error {
-	_, err := this.db.Exec(sql)
-	return err
 }
