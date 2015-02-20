b879f99c8ff414b1ea7ec4e896dcb475ce2b0673
Rex Lee <duguying2008@gmail.com>
2015-02-20 19:27:01 +0800

    CreateSession and GetSession

--- models/session.go
+++ models/session.go
@@ -2,6 +2,7 @@ package model
 
 import (
 	"github.com/astaxie/beego/orm"
+	"github.com/gogather/com"
 	"time"
 )
 
@@ -15,10 +16,27 @@ func (this *SessionTab) TableName() string {
 	return "session"
 }
 
-func (this *SessionTab) CreateSession() {
+// create session and add session into database
+func (this *SessionTab) CreateSession() (string, error) {
+	sid := com.CreateGUID()
+	o := orm.NewOrm()
+	var sess SessionTab
 
+	sess.Session = sid
+	sess.CreateTime = time.Now()
+
+	_, err = o.Insert(&sess)
+
+	return sid, err
 }
 
-func (this *SessionTab) GetSession(sid string) {
+// get session from database
+func (this *SessionTab) GetSession(sid string) (Session, error) {
+	o := orm.NewOrm()
+	var sess SessionTab
 
+	sess.Session = sid
+	err = o.Read(&sess, "Session")
+
+	return sess, err
 }
