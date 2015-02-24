5136343275f9976cb2fd030c44157e02b2ba6165
Rex Lee <duguying2008@gmail.com>
2015-02-24 17:31:02 +0800

    change client test

--- test/client_test.go
+++ test/client_test.go
@@ -3,23 +3,30 @@ package test
 import (
 	"fmt"
 	"github.com/duguying/judger/client"
-	"github.com/gogather/com"
 	"testing"
 )
 
 func Test_TCP(t *testing.T) {
-	cli, _ := client.New("127.0.0.1", 1004)
+	cli, err := client.New("duguying.net", 1004, "123456789")
 
-	msg1 := com.ReadFile("login.json")
-	resp1, _ := cli.Request(msg1)
-	fmt.Println(resp1)
+	if err != nil {
+		fmt.Println(err)
+	}
 
-	msg2 := com.ReadFile("task.json")
-	resp2, _ := cli.Request(msg2)
-	fmt.Println(resp2)
+	response, err := cli.AddTask(12, "randomstring", "C", "int main(){return 0;}")
 
-	msg3 := com.ReadFile("info.json")
-	resp3, _ := cli.Request(msg3)
-	fmt.Println(resp3)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(response)
+	}
+
+	response, err = cli.GetStatus(12, "randomstring")
+
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(response)
+	}
 
 }
